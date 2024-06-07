13b8c244feee3ed9e906a2ba27b5f1c7ebc692dc
edertelhado <snekoyoru@gmail.com>
2024-06-07 19:55:01 -0300

    first commit

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineOptions({
+  name: "Welcome"
+});
+</script>
+
+<template>
+  <h1>Pure-Admin-Thin（非国际化版本）</h1>
+  <el-date-picker
+    v-model="value2"
+    type="date"
+    placeholder="Pick a day"
+    :disabled-date="disabledDate"
+    :shortcuts="shortcuts"
+    :size="size"
+  />
+</template>
